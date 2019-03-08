83e70c4851f81d0104a6ecb98d5ff860260d45d4
Johan Bos <johan.bos@kpn.com>
2019-03-08 14:51:18 +0100

    iOS: prepared component view controller with page loading

--- pirates-ios/PiratesGame/Page/ViewControllers/SplashViewController.swift
+++ pirates-ios/PiratesGame/Page/ViewControllers/SplashViewController.swift
@@ -1,23 +1,112 @@
 //
 //  SplashViewController.swift
-//  Pirates Game
-//
-//  Created by Johan Bos on 08/03/2019.
-//  Copyright © 2019 crescentflare. All rights reserved.
+//  View controller: the splash screen, loading assets
 //
 
 import UIKit
+import ViewletCreator
 
-class SplashViewController: UIViewController {
+class SplashViewController: ComponentViewController, AppEventObserver, PageLoaderContinuousCompletion {
+
+    // --
+    // MARK: Outlets
+    // --
     
-    override func loadView() {
-        view = UIView()
-        view.backgroundColor = UIColor.red
-    }
+    private let pageJson = "splash.json"
+    private var pageLoader: PageLoader?
+    private var hotReloadPageUrl = ""
+    private let containerView = FrameContainerView()
+    private var modules = [ControllerModule]()
 
+
+    // --
+    // MARK: Lifecycle
+    // --
+    
     override func viewDidLoad() {
+        // Set navigation bar
         super.viewDidLoad()
+        let navigationBar = TransparentNavigationBar()
+        navigationBar.isLightContent = true
+        navigationBarView = navigationBar
+        
+        // Set container
+        contentView = containerView
+        containerView.eventObserver = self
+        
+        // Add module
+        modules.append(AlertModule())
+        for module in modules {
+            module.didCreate(viewController: self)
+        }
+        
+        // Start loading
+        startContinuousPageLoad()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        let wasHotReloadPageUrl = hotReloadPageUrl
+        if !CustomAppConfigManager.currentConfig().devServerUrl.isEmpty && CustomAppConfigManager.currentConfig().enablePageHotReload {
+            hotReloadPageUrl = CustomAppConfigManager.currentConfig().devServerUrl
+            if !hotReloadPageUrl.hasPrefix("http") {
+                hotReloadPageUrl = "http://\(hotReloadPageUrl)"
+            }
+            hotReloadPageUrl = "\(hotReloadPageUrl)/pages/\(pageJson)"
+        } else {
+            hotReloadPageUrl = ""
+        }
+        if wasHotReloadPageUrl != hotReloadPageUrl || pageLoader == nil {
+            pageLoader = PageLoader(location: !hotReloadPageUrl.isEmpty ? hotReloadPageUrl : pageJson.replacingOccurrences(of: ".json", with: ""))
+            PageCache.shared.removeEntry(cacheKey: wasHotReloadPageUrl)
+            PageCache.shared.removeEntry(cacheKey: pageJson)
+        }
+        EventReceiverTool.shared.addObserver(addObserver: self)
+        startContinuousPageLoad()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        EventReceiverTool.shared.removeObserver(removeObserver: self)
+        stopContinuousPageLoad()
     }
 
-}
 
+    // --
+    // MARK: Interaction
+    // --
+    
+    func observedEvent(_ event: AppEvent, sender: Any?) {
+        for module in modules {
+            if module.catchEvent(event, sender: sender) {
+                break
+            }
+        }
+    }
+
+
+    // --
+    // MARK: Page loader integration
+    // --
+    
+    private func startContinuousPageLoad() {
+        pageLoader?.startLoadingContinuously(completion: self)
+    }
+    
+    private func stopContinuousPageLoad() {
+        pageLoader?.stopLoadingContinuously()
+    }
+    
+    func didUpdatePage(page: Page) {
+        let binder = ViewletDictBinder()
+        let inflateLayout: [String: Any] = [
+            "viewlet": "frameContainer",
+            "width": "stretchToParent",
+            "height": "stretchToParent",
+            "recycling": true,
+            "items": [page.layout]
+        ]
+        ViewletUtil.assertInflateOn(view: containerView, attributes: inflateLayout, binder: binder)
+    }
+    
+}
