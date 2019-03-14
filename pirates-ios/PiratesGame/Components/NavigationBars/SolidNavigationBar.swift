f2a628691c1ec74af9d7eda145c007861d7212a6
Johan Bos <johan.bos@kpn.com>
2019-03-14 15:35:01 +0100

    iOS: minor code style fix

--- pirates-ios/PiratesGame/Components/NavigationBars/SolidNavigationBar.swift
+++ pirates-ios/PiratesGame/Components/NavigationBars/SolidNavigationBar.swift
@@ -130,9 +130,9 @@ class SolidNavigationBar: FrameContainerView, NavigationBarComponent {
     // --
     
     private func pickBestForegroundColor(backgroundColor: UIColor, lightForegroundColor: UIColor, darkForegroundColor: UIColor) -> UIColor {
-        var red : CGFloat = 0
-        var green : CGFloat = 0
-        var blue : CGFloat = 0
+        var red: CGFloat = 0
+        var green: CGFloat = 0
+        var blue: CGFloat = 0
         var alpha: CGFloat = 0
         if backgroundColor.getRed(&red, green: &green, blue: &blue, alpha: &alpha) {
             var colorComponents = [blue, green, red]
